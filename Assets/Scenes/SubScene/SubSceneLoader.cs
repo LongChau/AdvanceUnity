c31f502c2fbca2795aa6a8c37f93f512c7a56882
LongChau1210 <longchau1210@gmail.com>
2020-09-24 00:14:24 +0700

    ~ update ~ test subscene

---
+++ Assets/Scenes/SubScene/SubSceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Scenes;
+using UnityEngine;
+
+namespace TestPerformance
+{
+    public class SubSceneLoader : ComponentSystem
+    {
+        private SceneSystem _sceneSystem;
+
+        protected override void OnUpdate()
+        {
+
+        }
+
+
+    }
+}
