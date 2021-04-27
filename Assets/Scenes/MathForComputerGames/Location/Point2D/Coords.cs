4c5b3383f2136ec36bd62896c3ca9ce4c78854a1
LongChau <longchau1210@gmail.com>
2021-04-27 10:02:12 +0700

    ~ update

---
+++ Assets/Scenes/MathForComputerGames/Location/Point2D/Coords.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityAdvance.Location
+{
+    public class Coords
+    {
+        float x;
+        float y;
+        float z;
+
+        public Coords(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = -1;
+        }
+
+        public Coords(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"({x},{y},{z})";
+        }
+
+        static public void DrawPoint(Coords position, float width, Color colour)
+        {
+            GameObject line = new GameObject($"Point_{position.ToString()}");
+            LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+            lineRenderer.material.color = colour;
+            lineRenderer.positionCount = 2;
+            // Draw a Rhombus - con thoi. 
+            lineRenderer.SetPosition(0, new Vector3(position.x - width/3.0f, position.y - width/3.0f, position.z));
+            lineRenderer.SetPosition(1, new Vector3(position.x + width / 3.0f, position.y + width / 3.0f, position.z));
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+        }
+
+        static public void DrawLine(Coords start, Coords end, float width, Color colour)
+        {
+            GameObject line = new GameObject($"Point_{start.ToString()}");
+            LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+            lineRenderer.material.color = colour;
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPosition(0, new Vector3(start.x, start.y, start.z));
+            lineRenderer.SetPosition(1, new Vector3(end.x, end.y, end.z));
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+        }
+
+        static public void DrawLine(Coords[] points, float width, Color colour)
+        {
+            GameObject line = new GameObject($"Point_{points.ToString()}");
+            LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+            lineRenderer.material.color = colour;
+            lineRenderer.positionCount = points.Length;
+            for (int i = 0; i < points.Length; i++)
+            {
+                var point = points[i];
+                lineRenderer.SetPosition(i, new Vector3(point.x, point.y, point.z));
+            }
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+        }
+    }
+}
---
+++ Assets/Scenes/MathForComputerGames/Location/Point2D/DrawLines.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityAdvance.Location
+{
+    public class DrawLines : MonoBehaviour
+    {
+        Coords point = new Coords(10, 20);
+
+        Coords startPointYAxis = new Coords(0, 100);
+        Coords endPointYAxis = new Coords(0, -100);
+
+        Coords startPointXAxis = new Coords(160, 0);
+        Coords endPointXAxis = new Coords(-160, 0);
+
+        Coords[] points = new Coords[]
+        {
+            new Coords(0, 0),
+            new Coords(20, 30),
+            new Coords(50, 30),
+            new Coords(60, 30),
+            new Coords(70, 60),
+            new Coords(55, 60),
+            new Coords(40, -50),
+            new Coords(-40, 10),
+            new Coords(10, 10),
+        };
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log(point.ToString());
+            Coords.DrawPoint(new Coords(0, 0), 2, Color.red);
+            Coords.DrawPoint(point, 2, Color.green);
+
+            Coords.DrawLine(startPointYAxis, endPointYAxis, 1, Color.green);
+            Coords.DrawLine(startPointXAxis, endPointXAxis, 1, Color.red);
+            Coords.DrawLine(points, 1, Color.white);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
