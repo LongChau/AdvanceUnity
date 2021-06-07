7c89c0c543e338d067faa30612262d559ac3427f
LongChau1210 <longchau1210@gmail.com>
2021-06-07 18:34:46 +0700

    + Add 2D A*

---
+++ Assets/Scenes/2DPathfinding/GridController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding
+{
+    public class GridController : MonoBehaviour
+    {
+        [SerializeField]
+        private NodeController _nodePrefab;
+        [SerializeField]
+        private List<NodeController> _listNodes;
+
+        [SerializeField]
+        private Vector2 _cellSize = Vector2.one;
+        [SerializeField]
+        private Vector2 _spacing = Vector2.zero;
+
+        [SerializeField]
+        private int _totalRow;
+        [SerializeField]
+        private int _totalColumn;
+
+        public List<NodeController> ListNodes => _listNodes;
+
+        public Vector2 CellSize => _cellSize;
+
+        public NodeController[,] Nodes = new NodeController[0, 0];
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _listNodes = new List<NodeController>();
+            Nodes = new NodeController[_totalRow, _totalColumn];
+            // Create grid map.
+            for (int columnIndex = 0; columnIndex < _totalRow; columnIndex++)
+            {
+                for (int rowIndex = 0; rowIndex < _totalColumn; rowIndex++)
+                {
+                    var node = Instantiate(_nodePrefab, this.transform);
+                    node.transform.position = new Vector2(rowIndex * _cellSize.x + _spacing.x, columnIndex * _cellSize.y + _spacing.y);
+                    node.name = $"Node_{rowIndex}_{columnIndex}";
+                    node.NodeData.Row = rowIndex;
+                    node.NodeData.Column = columnIndex;
+                    _listNodes.Add(node);
+                    Nodes[rowIndex, columnIndex] = node;
+                }
+            }
+        }
+
+        public NodeController GetNodeCtrl(int column, int row)
+        {
+            bool isValidColumn = (0 <= column && column < _totalColumn);
+            bool isValidRow = (0 <= row && row < _totalRow);
+            if (!isValidColumn || !isValidRow) return null;
+            return Nodes[column, row];
+        }
+    }
+}
---
+++ Assets/Scenes/2DPathfinding/NodeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding
+{
+    public class NodeController : MonoBehaviour
+    {
+        [SerializeField]
+        private Node _node;
+
+        [SerializeField]
+        private Sprite _normalSprite;
+        [SerializeField]
+        private Sprite _destinationSprite;
+        [SerializeField]
+        private Sprite _pathSprite;
+
+        [SerializeField]
+        private SpriteRenderer _render;
+
+        public Node NodeData
+        {
+            get => _node;
+            set
+            {
+                _node = value;
+            }
+        }
+
+        public NodeController ParentNode { get; set; }
+
+        public float X => transform.position.x;
+        public float Y => transform.position.y;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _node.UID = System.Guid.NewGuid().ToString();
+        }
+
+        private void OnMouseDown()
+        {
+            
+        }
+
+        //public bool IsTheSame(NodeController comparedNode)
+        //{
+        //    return _node.Column == comparedNode.GetNodeData.Column && _node.Row == comparedNode.GetNodeData.Row;
+        //}
+
+        public bool IsTheSame(NodeController comparedNode) => _node.UID == comparedNode.NodeData.UID;
+
+        internal void ChangeToDestinationColor()
+        {
+            _render.sprite = _destinationSprite;
+        }
+
+        internal void ChangeToPathColor()
+        {
+            _render.sprite = _pathSprite;
+        }
+    }
+}
