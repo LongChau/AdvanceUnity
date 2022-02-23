fb91035efb181d4926d4b3e3a03db327b167d10f
LongChau1210 <longchau1210@gmail.com>
2022-02-23 22:18:00 +0700

    ~ update test particle emit

---
+++ Assets/Scenes/ParticalSystem/SelectableCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class SelectableCube : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem _particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Select {gameObject.name}");
+        EmitParams param = new EmitParams();
+        param.position = transform.position;
+        _particleSystem.Emit(param, 1);
+    }
+}
